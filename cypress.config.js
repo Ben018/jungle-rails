f7843a25cacf11638051575788f26a402fd85c3c
Benson <bensonwlee@gmail.com>
2023-08-29 15:26:06 -0400

    add feature testing-home-page

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,19 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
+      on("before:browser:launch", (browser = {}, launchOptions) => {
         /* ... */
-      })
+      });
     },
   },
+  baseUrl: "http://localhost:3000",
+  viewportWidth: 1280,
+  viewportHeight: 720,
   screenshotsFolder: "tmp/cypress_screenshots",
   videosFolder: "tmp/cypress_videos",
-  trashAssetsBeforeRuns: false
-})
+  trashAssetsBeforeRuns: false,
+});
---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe("example to-do app", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
