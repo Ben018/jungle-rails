cbaaf55fcdfe176b7f52e1917a4b02da353d8402
Benson <bensonwlee@gmail.com>
2023-08-29 15:58:09 -0400

    add feature testing-product-page

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,4 +1,4 @@
-describe("Homepage", () => {
+describe("Homepage ", () => {
   beforeEach(() => {
     cy.visit("/");
   });
@@ -9,5 +9,10 @@ describe("Homepage", () => {
 
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
+  });
+
+  it("Click on product takes you to the product details", () => {
+    cy.get(".products article:first-child").click();
+    cy.get(".product-detail").should("be.visible");
   });
 });
